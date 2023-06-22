61607ec677f4edd5d9c0f43ebd4021d9c89c2218
Yaswanth Sirigiri <93373615+yaswanths2002@users.noreply.github.com>
2023-06-22 12:02:24 +0530

    Updated files

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: 1,
+    name: "Video File",
+    type: "video",
+    path: "/file-server/Munnar.mp4",
+    size: "61441",
+  },
+  {
+    id: 2,
+    name: "Audio File",
+    type: "audio",
+    path: "/file-server/Closer.mp3",
+    size: "9560",
+  },
+  {
+    id: 3,
+    name: "Document File",
+    type: "document",
+    path: "/file-server/SQL.pdf",
+    size: "755",
+  },
+  {
+    id: 4,
+    name: "Image File-1",
+    type: "image",
+    path: "/file-server/photo_1.jpg",
+    size: "137",
+  },
+  {
+    id: 5,
+    name: "Image File-2",
+    type: "image",
+    path: "/file-server/photo_2.jpg",
+    size: "175",
+  },
+  {
+    id: 6,
+    name: "Image File-3",
+    type: "image",
+    path: "/file-server/photo_3.jpg",
+    size: "4479",
+  },
+];
